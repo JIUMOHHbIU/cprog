4c2d18046d7e0da87429be8c1f79f9e065dd0a5c
Zhilyaev Anton <zhilyaevam@student.bmstu.ru>
2023-02-20 15:06:35 +0300

    added user input check task 5

--- lab_01_05_00/main.c
+++ lab_01_05_00/main.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
 
 #define ERR_NO 0
+#define ERR_INPUT 1
 
 int my_pow(int, unsigned);
+
+int input_integer(int *number);
+
+int input_unsigned_integer(unsigned int *number);
 
 int main(void)
 {
     int a;
     unsigned n;
-    scanf("%d%u", &a, &n);
+    int rc = ERR_NO;
 
-    printf("%d", my_pow(a, n));
+    if (input_integer(&a))
+    {
+        rc = ERR_INPUT;
+    }
+    if (rc == ERR_NO)
+    {
+        if (input_unsigned_integer(&n))
+        {
+            rc = ERR_INPUT;
+        }
+    }
+
+    if (rc == ERR_NO)
+    {
+        printf("%d", my_pow(a, n));
+    }
+
+    return rc;
 }
 
 int my_pow(int a, unsigned n)
@@ -22,4 +44,24 @@ int my_pow(int a, unsigned n)
     }
 
     return result;
+}
+
+int input_integer(int *number)
+{
+    char tmp;
+    int rc;
+
+    int success = (((rc = scanf("%d%c", number, &tmp)) != 2 && rc != EOF) || tmp != '\n');
+
+    return success;
+}
+
+int input_unsigned_integer(unsigned *number)
+{
+    char tmp;
+    int rc;
+
+    int success = (((rc = scanf("%u%c", number, &tmp)) != 2 && rc != EOF) || tmp != '\n');
+
+    return success;
 }
